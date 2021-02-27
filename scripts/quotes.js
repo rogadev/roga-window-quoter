1325f3c9a5d718bacc3e4ecd8ca4cbfc5b92edd8
vonroga <ryan@mossaway.ca>
2021-02-27 14:41:15 -0800

    Update quotes.js

--- scripts/quotes.js
+++ scripts/quotes.js
@@ -8,19 +8,19 @@
  */
 
 const panePrices = {
-    uk: 18.00,
-    ul: 10.00,
-    um: 8.00,
-    us: 6.00,
-    mf: 2.00,
-    ml: 6.00,
-    mm: 4.00,
-    ms: 3.00,
-    gf: 1.00,
-    gl: 5.00,
-    gm: 3.00,
-    gs: 2.00
-}
+  uk: 18.0,
+  ul: 10.0,
+  um: 8.0,
+  us: 6.0,
+  mf: 2.0,
+  ml: 6.0,
+  mm: 4.0,
+  ms: 3.0,
+  gf: 1.0,
+  gl: 5.0,
+  gm: 3.0,
+  gs: 2.0,
+};
 
 /**
  * @class   LineItem    Represents each individual line item object in the quote.
@@ -31,44 +31,42 @@ const panePrices = {
  * @method      outputString()  Returns the HTML ready string for the table row this line item occupies.
  */
 class LineItem {
+  constructor(paneType) {
+    (this.paneType = paneType),
+      (this.numberOf = 1),
+      (this.priceOfEach = this.getPriceOf(this.paneType));
+    this.price = this.calculatePrice();
+    this.outputString = function () {
+      let output =
+        "<tr>" +
+        `<td>${this.paneType}</td>` +
+        `<td>${this.numberOf}</td>` +
+        `<td>${this.getPrice()}</td>` +
+        `<td>${this.calculatePrice()}</td>` +
+        "</tr>";
+      return output;
+    };
+  }
 
-    constructor(paneType) {
-        this.paneType = paneType,
-        this.numberOf = 1,
-        this.priceOfEach = this.getPriceOf(this.paneType);
-        this.price = this.calculatePrice();
-        this.outputString = function () {
-            let output =
-                "<tr>" +
-                    `<td>${this.paneType}</td>` +
-                    `<td>${this.numberOf}</td>` +
-                    `<td>${this.getPrice()}</td>` +
-                    `<td>${this.calculatePrice()}</td>` +
-                "</tr>";
-            return output;
-        };
+  calculatePrice = function () {
+    return Number.parseFloat(this.priceOfEach * this.numberOf).toFixed(2);
+  };
+
+  getPrice = function () {
+    return Number.parseFloat(this.priceOfEach).toFixed(2);
+  };
+
+  getPriceOf = function (paneType) {
+    for (let [key, value] of Object.entries(panePrices)) {
+      if (paneType == key) {
+        return value;
+      }
     }
+  };
 
-    calculatePrice = function() {
-        return Number.parseFloat(this.priceOfEach * this.numberOf).toFixed(2);
-    }
-
-    getPrice = function() {
-        return Number.parseFloat(this.priceOfEach).toFixed(2);
-    }
-
-    getPriceOf = function(paneType) {
-        for (let [key, value] of Object.entries(panePrices)) {
-            if (paneType == key) {
-                return value;
-            }
-          }
-    }
-
-    increasePaneCount = function() {
-        this.numberOf++;
-    }
-
+  increasePaneCount = function () {
+    this.numberOf++;
+  };
 }
 
 /**
@@ -77,13 +75,13 @@ class LineItem {
 const entrySection = document.getElementById("current-entry-section");
 const collectionSection = document.getElementById("entry-list-section");
 
-// set up delete and counter button 
-document.querySelectorAll("button").forEach(element => {
-    if(element.classList.contains("delete")){
-        element.addEventListener("click", e => deleteLine(e.target));
-    } else {
-        element.addEventListener("click", e => countPane(e.target));
-    };
+// set up delete and counter button
+document.querySelectorAll("button").forEach((element) => {
+  if (element.classList.contains("delete")) {
+    element.addEventListener("click", (e) => deleteLine(e.target));
+  } else {
+    element.addEventListener("click", (e) => countPane(e.target));
+  }
 });
 // for use in button click events
 var currentPaneSelected, lastPaneSelected;
@@ -98,47 +96,48 @@ var currentLineItem = new LineItem();
  * @param {string} elementId The element ID to be effected.
  */
 function toggleDisplay(elementId) {
-    let elementToToggle = document.getElementById(elementId);
-    if (elementToToggle.style.display == "none") {
-        elementToToggle.style.display = "block";
-    } else {
-        elementToToggle.style.display = "none";
-    }
+  let elementToToggle = document.getElementById(elementId);
+  if (elementToToggle.style.display == "none") {
+    elementToToggle.style.display = "block";
+  } else {
+    elementToToggle.style.display = "none";
+  }
 }
 
-function countPane (element) {
-    currentPaneSelected = element.id;
+function countPane(element) {
+  currentPaneSelected = element.id;
 
-    if (lastPaneSelected == currentPaneSelected) {
-        // Selection multiple of current pane. Increase current line item pane count.
-        currentLineItem.increasePaneCount();
-    } else {
-        // Selecting a new pane time, so start a new line item.
-        if (currentLineItem.paneType !== undefined) addLineItemToQuote(currentLineItem.outputString());
-        currentLineItem = new LineItem(element.id);
-    }
+  if (lastPaneSelected == currentPaneSelected) {
+    // Selection multiple of current pane. Increase current line item pane count.
+    currentLineItem.increasePaneCount();
+  } else {
+    // Selecting a new pane time, so start a new line item.
+    if (currentLineItem.paneType !== undefined)
+      addLineItemToQuote(currentLineItem.outputString());
+    currentLineItem = new LineItem(element.id);
+  }
 
-    updateCurrentLineItem();
+  updateCurrentLineItem();
 
-    // lastly, set the last pane selected variable equal to this current selection
-    lastPaneSelected = currentPaneSelected;
+  // lastly, set the last pane selected variable equal to this current selection
+  lastPaneSelected = currentPaneSelected;
 }
 
 function addLineItemToQuote(newLineItem) {
+  collectionSection.style.display = "block";
 
-    collectionSection.style.display = "block";
+  let outputString = "";
+  quoteLineItems.push(newLineItem);
 
-    let outputString = "";
-    quoteLineItems.push(newLineItem);
+  for (const lineItem of quoteLineItems) {
+    outputString += lineItem;
+  }
 
-    for (const lineItem of quoteLineItems) {
-        outputString += lineItem;
-    }
-
-    quoteTable.innerHTML = outputString;
+  quoteTable.innerHTML = outputString;
 }
 
 function updateCurrentLineItem() {
-    entrySection.style.display = "block";
-    currentLineItemTable.innerHTML = currentLineItem.outputString();
+  entrySection.style.display = "block";
+  // todo: output table header
+  currentLineItemTable.innerHTML = currentLineItem.outputString();
 }
