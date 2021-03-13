840c69d6fe8ccff10a3627d83add9e2c57c0e802
vonroga <ryan@mossaway.ca>
2021-03-12 18:19:32 -0800

    Pricing Page

---
+++ scripts/pricing.js
@@ -0,0 +1,8 @@
+"use strict";
+
+/**
+ * pricing.js
+ * @author  Ryan Paranich   <rparanich@northislandcollege.ca>
+ * Handles the pricing page features and functions. Using session storage to store adjusted
+ * pricing, while using a default price list as a base line.
+ */
